9c8bd05d00550e593c174f2a629395bb452941d7
SrLucasGaldinor <lucasgaldinorosa2904@gmail.com>
2026-07-18 16:45:27 -0300

    feat: Adiciona model User e configura dependencias no pom.xml

---
+++ src/main/java/com/empresa/gestao_usuarios/model/User.java
@@ -0,0 +1,38 @@
+package com.empresa.gestao_usuarios.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private String role = "USER";
+}
